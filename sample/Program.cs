6361ee09ae5b233142e9264939093a5bba1be9ff
stefc <stefan.boether@gmail.com>
2022-12-27 14:13:30 +0100

    feat: add account.login & account.logout methods to the library

---
+++ sample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Immutable;
+using System.Globalization;
+
+using Microsoft.Extensions.Configuration;
+
+using stefc.inwx;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+			.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+			.AddJsonFile("appsettings.json")
+			.AddUserSecrets<Program>()
+			.Build();
+
+        var user = config["inwx-user"] ?? "<unknown>";
+        var password = config["inwx-password"] ?? "<unknown>";
+
+        ApiClient apiClient = new ApiClient(ApiClient.API_URL_LIVE, Language.EN);
+        var response = await apiClient.AccountLogin(user,password, false); 
+
+        System.Console.WriteLine($"Customer-No:{response.customerNo}");
+
+        await apiClient.AccountLogout();
+    }
+
+    
+}
--- src/ApiClient.cs
+++ src/ApiClient.cs
@@ -6,6 +6,8 @@ using System.Net.Http;
 
 using System.Threading.Tasks;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.Http.Headers;
 
 namespace stefc.inwx
 {
@@ -22,6 +24,8 @@ namespace stefc.inwx
         private readonly string apiUrl;
         private readonly string language;
 
+        private readonly HttpClient httpClient;
+
         public static string generateClientTransactionId()
         {
             return $"DomRobot-{new Random().Next(1000000000)}";
@@ -31,6 +35,10 @@ namespace stefc.inwx
         {
             this.apiUrl = apiUrl;
             this.language = lang == Language.DE ? "DE" : "EN";
+            this.httpClient = BuildClient();
+            this.httpClient.BaseAddress = new Uri(this.apiUrl);
+            this.httpClient.DefaultRequestHeaders.Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         public IWebProxy Proxy { get; private set; } = null;
@@ -38,27 +46,58 @@ namespace stefc.inwx
         private HttpClient BuildClient()
         {
             if (this.Proxy == null)
-                return new HttpClient();
+            {
+                var client = new HttpClient();
+                
+                return client;
+            }
 
             var handler = new HttpClientHandler();
             handler.Proxy = this.Proxy;
+            
             return new HttpClient(handler);
         }
 
 
         public record ApiRequest<T>(string method, T @params) { };
-        public record LoginParams(string user, string pass) { };
+        public record class Envelope<T>(int code, string msg, T resData) {};
+        public record Unit() { };
+
+        public record LoginParams(string user, string pass, [property: JsonPropertyName("case-insensitive")] bool caseInsensitive) { };
         public record LoginResponse(int customerId, int customerNo, int accountId, string tfa, string buildDate, int version) { }
         // "method": "account.login",
         // "params": {"user":"blakeks", "pass":"SCHEDULED", "lang":"de", "clTRID":"CLIENT-123123"}
 
-        public async Task<LoginResponse> AccountLogin(string user, string password)
+        public async Task<LoginResponse> AccountLogin(string user, string password, bool caseInsensitive = false)
         {
-
-            var login = new ApiRequest<LoginParams>("account.login", new LoginParams(user, password));
+            var login = new ApiRequest<LoginParams>("account.login", new LoginParams(user, password, caseInsensitive));
             var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
-            var response = await BuildClient().PostAsJsonAsync<ApiRequest<LoginParams>>("https://localhost:12345/stocks/", login, options);
-            return await response.Content.ReadFromJsonAsync<LoginResponse>();
+            var response = await this.httpClient.PostAsJsonAsync<ApiRequest<LoginParams>>(string.Empty, login, options);
+            response = response.EnsureSuccessStatusCode();
+            if (response != null) {
+                var result = await response.Content.ReadFromJsonAsync<Envelope<LoginResponse>>();
+                if (result.code != 1000) 
+                    throw new Exception(result.msg);
+                return result?.resData;
+            }
+            else throw new Exception(response.ToString());
         }
+
+        public async Task<Unit> AccountLogout() 
+        {
+            var logout = new ApiRequest<Unit>("account.logout", new Unit());
+            var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+            var response = await this.httpClient.PostAsJsonAsync<ApiRequest<Unit>>(string.Empty, logout, options);
+            response = response.EnsureSuccessStatusCode();
+            if (response != null) {
+                var result = await response.Content.ReadFromJsonAsync<Envelope<Unit>>();
+                if (result.code != 1500) 
+                    throw new Exception(result.msg);
+                return result?.resData;
+            }
+            else throw new Exception(response.ToString());
+        }
+
+
     }
 }
